657aaad870e60ff372f80332115c93008f323d03
Pavel Korzhenko <pksmall@live.ru>
2018-03-27 21:08:39 +0300

    определились со столкновениями и стенами.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,16 +16,9 @@ namespace Snake
             Console.SetBufferSize(80, 25);
             Console.SetWindowSize(80, 25);
 
-            // рисуем рамочку
-            HorizontalLines xLine1 = new HorizontalLines(0, 78, 0, '+');
-            HorizontalLines xLine2 = new HorizontalLines(0, 78, 24, '+');
-            xLine1.Draw();
-            xLine2.Draw();
-
-            VerticalLines yLine1 = new VerticalLines(0, 24, 0, '+');
-            VerticalLines yLine2 = new VerticalLines(0, 24, 78, '+');
-            yLine1.Draw();
-            yLine2.Draw();
+            // стены
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
 
             // отрисовка точек
             Point p = new Point(4, 5, '*');
@@ -38,6 +31,11 @@ namespace Snake
 
             while(true)
             {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+
                 if (snake.Eat(food))
                 {
                     food = foodCreator.CreateFood();
---
+++ Snake/Snake/Walls.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Walls
+    {
+        List<Figure> wallList;
+
+        public Walls(int mapWidth, int mapHeight)
+        {
+            wallList = new List<Figure>();
+
+            // рисуем рамочку
+            HorizontalLines xLine1 = new HorizontalLines(0, mapWidth - 2, 0, '+');
+            HorizontalLines xLine2 = new HorizontalLines(0, mapWidth -2, mapHeight - 1, '+');
+            VerticalLines yLine1 = new VerticalLines(0, mapHeight - 1, 0, '+');
+            VerticalLines yLine2 = new VerticalLines(0, mapHeight - 1, mapWidth - 2, '+');
+
+            wallList.Add(xLine1);
+            wallList.Add(xLine2);
+            wallList.Add(yLine1);
+            wallList.Add(yLine2);
+        }
+        
+        internal bool IsHit(Figure figure)
+        {
+            foreach(var wall in wallList)
+            {
+                if (wall.IsHit(figure))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Draw()
+        {
+            foreach(var wall in wallList)
+            {
+                wall.Draw();
+            }
+        }
+    }
+}
