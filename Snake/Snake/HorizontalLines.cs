0a0925a19ad864b64adcb73d8755ed414d2ddfe1
Pavel Korzhenko <pksmall@live.ru>
2018-03-27 20:45:25 +0300

    применяем полиморфизм

--- Snake/Snake/HorizontalLines.cs
+++ Snake/Snake/HorizontalLines.cs
@@ -18,5 +18,14 @@ namespace Snake
                 pList.Add(p);
             }
         }
+
+        public override void Draw()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            base.Draw();
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
     }
 }
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,45 +16,30 @@ namespace Snake
             Console.SetBufferSize(80, 25);
             Console.SetWindowSize(80, 25);
 
-            // рисуем рамочку
-            HorizontalLines xLine1 = new HorizontalLines(0, 78, 0, '+');
-            HorizontalLines xLine2 = new HorizontalLines(0, 78, 24, '+');
-            xLine1.Draw();
-            xLine2.Draw();
+            VerticalLines v1 = new VerticalLines(0, 10, 5, '%');
+            Draw(v1);
 
-            VerticalLines yLine1 = new VerticalLines(0, 24, 0, '+');
-            VerticalLines yLine2 = new VerticalLines(0, 24, 78, '+');
-            yLine1.Draw();
-            yLine2.Draw();
-
-            // отрисовка точек
             Point p = new Point(4, 5, '*');
-            Snake snake = new Snake(p, 4, Direction.RIGHT);
-            snake.Draw();
+            Figure fSnake = new Snake(p, 4, Direction.RIGHT);
+            Draw(fSnake);
+            Snake snake = (Snake)fSnake;
 
-            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
-            Point food = foodCreator.CreateFood();
-            food.Draw();
+            HorizontalLines h1 = new HorizontalLines(0, 5, 6, '&');
 
-            while(true)
+            List<Figure> figures = new List<Figure>();
+            figures.Add(fSnake);
+            figures.Add(v1);
+            figures.Add(h1);
+
+            foreach(var f in figures)
             {
-                if (snake.Eat(food))
-                {
-                    food = foodCreator.CreateFood();
-                    food.Draw();
-                } else
-                {
-                    snake.Move();
-                }
-
-                Thread.Sleep(100);
-
-                if (Console.KeyAvailable)
-                {
-                    ConsoleKeyInfo key = Console.ReadKey();
-                    snake.HandleKey(key.Key);
-                }
+                f.Draw();
             }
+        }
+
+        static void Draw(Figure figure)
+        {
+            figure.Draw();
         }
     }
 }
